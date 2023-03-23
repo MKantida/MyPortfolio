d7beac6f7bf7b807e5d94070dddaebee7f2fb6c6
MKantida <97874677+MKantida@users.noreply.github.com>
2023-03-23 16:21:09 +0700

    2

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import './App.css';
-import Navbar from './components/Navbar';
+import Navbar from './components/Navbar'
 
-//contents
+// Contents
 import Home from './contents/Home'
+import About from './contents/About'
+import Education from './contents/Education'
+import Skills from './contents/Skills'
+import Contact from './contents/Contact'
 
 import { BrowserRouter as Router, Route } from 'react-router-dom'
-
 
 function App() {
   return (
@@ -14,7 +17,19 @@ function App() {
       <div className="App">
         <Navbar />
         <Route exact path="/">
-        <Home />
+          <Home />
+        </Route>
+        <Route exact path="/about">
+          <About />
+        </Route>
+        <Route exact path="/education">
+          <Education />
+        </Route>
+        <Route exact path="/skills">
+          <Skills />
+        </Route>
+        <Route exact path="/contact">
+          <Contact />
         </Route>
       </div>
     </Router>
