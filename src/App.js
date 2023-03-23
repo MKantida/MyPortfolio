1cb72cfd814cc515f9b2db00fe439b86be4c2ae6
MKantida <97874677+MKantida@users.noreply.github.com>
2023-03-23 15:36:33 +0700

    1
    1

--- src/App.js
+++ src/App.js
@@ -2,13 +2,20 @@ import React from 'react';
 import './App.css';
 import Navbar from './components/Navbar';
 
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+//contents
+import Home from './contents/Home'
+
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+
 
 function App() {
   return (
     <Router>
       <div className="App">
         <Navbar />
+        <Route exact path="/">
+          <Home />
+        </Route>
       </div>
     </Router>
   );
